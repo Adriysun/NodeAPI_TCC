cb0b18f885bad92a48224535218a02f40106d901
Ana Beatriz <105469302+ol-anaa@users.noreply.github.com>
2022-10-07 21:04:16 -0300

    .

--- Daos/usuarios.js
+++ Daos/usuarios.js
@@ -2,11 +2,11 @@ const db = require("../js/_database");
 
 async function inclua(usuario) {
     try {
-        const pg   = 'INSERT INTO usuario (cpf, id_email, nome, sobrenome, senha, dtnasci ) VALUES (?,?,?,?,?,?)';
+        const pg   = 'INSERT INTO usuario (cpf, id_email, nome, sobrenome, senha, dtnasci ) VALUES ($1, $2, $3, $4, $5, $6)';
         const dados = [usuario.CPF, usuario.Email, usuario.Nome, usuario.Sobrenome, usuario.Senha, usuario.DataNasci];
         await db.query (pg, dados);
         return true;
-    } catch (excecao) {
+    } catch(error) {
         return false;
     }
 }
