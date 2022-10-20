57db7891a2c3fdb4e4422d1cb90a9302d882c395
Adrian <adrian.rodrigues0806@gmail.com>
2022-10-20 13:38:43 -0300

    LOGIN E CADASTRO COM BCRYPT

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -3,52 +3,52 @@ const bcrypt = require ('bcrypt');
 
 // Método responsável pelo cadastramento de usuários
 
-exports.createUser = async (req, res) => {
+exports.createUser = async (req, res, next) => {
   const {cpf, email, nome, sobrenome, senha, dtnasci} = req.body;
-  bcrypt.hash(req.body.senha, 15, (errBcrypt, hash) =>{
-    if (errBcrypt) {return res.status(500).send({ error: errBcrypt})}
-      db.query('INSERT INTO usuario (cpf, email, nome, sobrenome, senha, dtnasci) VALUES ($1, $2, $3, $4, $5, $6)',
-      [cpf, email, nome, sobrenome, hash, dtnasci],
-      (error, results) => {
-        db.release();
-        if (error) {return res.status(500).send ({error: error})}
-        response = {
-          mensagem: 'Usuário criado com sucesso (AMEM)',
-          usuarioCriado:{
-            id_usuario: results.insertId,
-            cpf: req.body.cpf,
-            email: req.body.email,
-            nome: req.body.nome,
-            sobrenome: req.body.sobrenome,
-            dtnasci: req.body.dtnasci
-        }
-      }
-        return res.status(201).send(response);
-      })
-  });
-
-  /*
-    const { cpf, email, nome, sobrenome, senha, dtnasci} = req.body;
-    const { rows } = await db.query(
-      'INSERT INTO usuario (cpf, email, nome, sobrenome, senha, dtnasci) VALUES ($1, $2, $3, $4, $5, $6)',
-      [cpf, email, nome, sobrenome, senha, dtnasci]
-    );
-
-    res.status(201).send({
-        message: "Usuario cadastrado com sucesso!",
-        body: {
-          user: {cpf, email, nome, sobrenome, senha, dtnasci}
-        },
+ // db.query('SELECT * FROM usuario WHERE email = $1', [email], (error, results) =>{
+ //  if (error) {return res.status(500).send({ error: error })}
+ //  if (results.length > 0) {
+ //     res.status(409).send({ mensagem: 'Usuário já cadastrado' })
+ //  } else {
+      bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) =>{
+        if (errBcrypt) {return res.status(500).send({ error: errBcrypt})}
+          db.query('INSERT INTO usuario (cpf, email, nome, sobrenome, senha, dtnasci) VALUES ($1, $2, $3, $4, $5, $6)',
+          [cpf, email, nome, sobrenome, hash, dtnasci],
+          (error, results) => {
+          db.release();
+            if (error) {return res.status(500).send ({error: error})}
+            return res.status(201).send({
+              mensagem: 'Usuário criado com sucesso (AMEM)',
+              usuarioCriado:{cpf, email, nome, sobrenome, dtnasci }
+            });
+          })
       });
-
-      console.log("Usuário cadastrado com sucesso!");
-     */ 
-    };
-
-exports.loginUser = async (req, res) =>{
-  const user = user.findOne({
-    attributes: ['id_usuario, email, senha']
+      //console.log('Usuário cadastrado com sucesso!')
+    }
+ //})
+//}
+exports.loginUser = async (req, res, next) => {
+  const {email} = req.body;
+  const query = 'SELECT * FROM usuario WHERE email = $1';
+  db.query(query,[email], (error, results, fields) =>{
+    db.release();
+    if (error) {return res.status(500).send({ error: error })}
+//    if (results.length < 1) {
+//      return res.status(401).send({mensagem: 'Falha na autenticação'})
+//    }
+    bcrypt.compare(req.body.senha, results[0].senha, (err, result)=>{
+      if(err){
+        return res.status(401).send({ mensagem: 'Falha na autenticação2'})
+      }
+      if(result){ 
+        console.log('DEU TUDO CERTO');
+        return res.status(200).send({ mensagem: 'Autenticado com sucesso'});
+      }
+      return res.status(401).send({ mensagem: 'Falha na autenticação3'})
+    })
   })
+
+}
 
 
   
@@ -65,7 +65,7 @@ exports.loginUser = async (req, res) =>{
     });
   }
 */
-}
+
 
 // https://www.youtube.com/watch?v=A-c643zCW7E (login)
 // https://www.youtube.com/watch?v=aVAl8GzS0d0 (cadastro with hash)
