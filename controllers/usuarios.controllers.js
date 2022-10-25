0108eae0c9e09d84142b61df57b838365a596779
Adrian <adrian.rodrigues0806@gmail.com>
2022-10-25 20:54:03 -0300

    apenas tentativas

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -5,35 +5,38 @@ const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res, next) => {
   const { cpf, email, nome, sobrenome, senha, dtnasci } = req.body;
-  bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-    if (errBcrypt) { return res.status(500).send({ error: errBcrypt }); }
-    db.query('INSERT INTO usuario (cpf, email, nome, sobrenome, senha, dtnasci) VALUES ($1, $2, $3, $4, $5, $6)',
-      [cpf, email, nome, sobrenome, hash, dtnasci]);
+  await db.query('SELECT * FROM usuario WHERE email = $1', [req.body.email], (error, results) => {
+  if (error) { return res.status(500).send({ error: error }) }
+    if (results.length > 0) {
+      res.status(409).send({ mensagem: 'Usuário já cadastrado' })
+    } else {
+      bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+        if (errBcrypt) { return res.status(500).send({ error: errBcrypt }); }
+        db.query('INSERT INTO usuario (cpf, email, nome, sobrenome, senha, dtnasci) VALUES ($1, $2, $3, $4, $5, $6)',
+          [req.body.cpf, email, nome, sobrenome, hash, dtnasci]);
 
-      //(error, results) => {
-      //  db.release();
-      //  if (error) { return res.status(500).send({ error: error }) }
         return res.status(201).send({
           mensagem: 'Usuário criado com sucesso (AMEM)',
-      //    id_usuario: results.insertId,
           usuarioCriado: { cpf, email, nome, sobrenome, dtnasci }
         });
-     // });
+      });
+      console.log("Usuário cadastrado com sucesso!");
+    }
   });
 }
 
 
 
 
+
 exports.loginUser = async (req, res, next) => {
-  const { email } = req.body;
-  const query = 'SELECT * FROM usuario WHERE email = $1';
-  db.query(query, [email], (error, results, fields) => {
-    db.release();
-    if (error) { return res.status(500).send({ error: error }) }
-    //    if (results.length < 1) {
-    //      return res.status(401).send({mensagem: 'Falha na autenticação'})
-    //    }
+  const { email, senha } = req.body;
+  //const query = 'SELECT * FROM usuario WHERE email = $1';
+  const results = await db.query('SELECT * FROM usuario WHERE email = $1', [req.body.email]);
+    //if (error) { return res.status(500).send({ error: error }) }
+    if (results.length < 1) {
+          return res.status(401).send({mensagem: 'Falha na autenticação'})
+    }
     bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
       if (err) {
         return res.status(401).send({ mensagem: 'Falha na autenticação2' })
@@ -44,8 +47,6 @@ exports.loginUser = async (req, res, next) => {
       }
       return res.status(401).send({ mensagem: 'Falha na autenticação3' })
     })
-  })
-
 }
 
 
