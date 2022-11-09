cdcefbfbcc2df0ee9588ddacb79345b01c42adf2
Adrian <adrian.rodrigues0806@gmail.com>
2022-11-08 22:49:15 -0300

    asd

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Rotas da API
 app.use(rotaInicial);
 app.use('/cadastro', userRoute);
